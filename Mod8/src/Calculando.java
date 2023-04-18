e34924aa13b22c0307100ee438dbe071eb208df4
Vitorepf <83190724+Vitorepf@users.noreply.github.com>
2023-04-18 17:54:17 -0300

    update da classe calculndo

--- Mod8/src/Calculando.java
+++ Mod8/src/Calculando.java
@@ -1,11 +1,19 @@
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
 
+/**
+ * 
+ * @author Vitorepf
+ *
+ */
 public class Calculando {
 
 	private float resultado;
 
 	public Calculando(float nota1, float nota2, float nota3, float nota4) {
 		resultado = (nota1 + nota2 + nota3 + nota4) / 4;
-		JOptionPane.showMessageDialog(null, "A media das 4 notas  e:  " + resultado);
+
+		JOptionPane.showMessageDialog(null, "A media das 4 notas  e: " + new DecimalFormat(".#").format((resultado)));
 	}
 }
